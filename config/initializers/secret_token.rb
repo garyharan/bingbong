9989a34d97031a0ffe1da7e9789c975adecaead6
Gary Haran <gary.haran@gmail.com>
2011-04-20 16:19:21 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bingbong::Application.config.secret_token = '472bd859fb6fb6266f49856db08671bac10f5e7554723b0908002bd9670eb289717b94a144fa680517dc15647783b7f69fdf98004717f8dec5faf5ec08858c01'
