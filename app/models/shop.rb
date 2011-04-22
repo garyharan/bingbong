8519f49e1d12cdcdf6ada5cf341bc557e9b7a321
Gary Haran <gary.haran@gmail.com>
2011-04-22 12:13:05 -0400

    added shop owned_by?(user)

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,7 @@
 class Shop < ActiveRecord::Base
   belongs_to :user
+
+  def owned_by?(owner)
+    owner.id == user_id
+  end
 end
--- test/unit/shop_test.rb
+++ test/unit/shop_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class ShopTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "owned_by?(user) works" do
+    @user = Factory.build :user
+    @shop = Factory.build :shop, :user_id => @user.id
+    assert @shop.owned_by?(@user)
+
+    @not_mine = Factory.build :shop
+    assert @not_mine.owned_by?(@user) == false, "user should not own this shop"
   end
 end
