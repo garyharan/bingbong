0d1ed8f2d115fe763f6e462c5d0add320614c430
Gary Haran <gary.haran@gmail.com>
2011-04-26 11:20:42 -0400

    ensuring uniqueness of name.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ActiveRecord::Base
   belongs_to :shop
+  validates_uniqueness_of :name, :scope => :shop_id
 end
--- test/unit/category_test.rb
+++ test/unit/category_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "names are unique within each shop" do
+    shop   = Shop.create Factory.attributes_for(:shop)
+    pizzas_1 = Category.create :name => "Pizzas", :shop_id => shop.id
+    pizzas_2 = Category.create :name => "Pizzas", :shop_id => shop.id
+    assert pizzas_1.valid?
+    assert !pizzas_2.valid?
   end
 end
