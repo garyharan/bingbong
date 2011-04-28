6a355aecdf24a2bad021a8f945e7c7d191a55ab8
Gary Haran <gary.haran@gmail.com>
2011-04-28 11:57:34 -0400

    added dependent destruction

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   belongs_to :shop
-  has_many :items
+  has_many :items, :dependent => :destroy
 
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => :shop_id
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :category
-  has_many :sizes
+  has_many :sizes, :dependent => :destroy
 
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => :category_id
