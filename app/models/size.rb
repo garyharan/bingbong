1e8d9695519995a524e0c6062250be2409199fb0
Gary Haran <gary.haran@gmail.com>
2011-04-29 14:37:03 -0400

    added automatic item.

--- app/models/size.rb
+++ app/models/size.rb
@@ -1,3 +1,9 @@
 class Size < ActiveRecord::Base
   belongs_to :category
+
+  def after_create
+    category.products.each do |product|
+      Item.create :product_id => product.id, :size_id => id
+    end
+  end
 end
--- test/unit/size_test.rb
+++ test/unit/size_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class SizeTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  setup do
+    @shop      = Factory.create :shop
+    @category  = @shop.categories.create Factory.attributes_for(:category, :shop_id      => @shop.id)
+    @product_1 = @category.products.create Factory.attributes_for(:product, :category_id => @category.id)
+    @product_2 = @category.products.create Factory.attributes_for(:product, :category_id => @category.id)
+  end
+
+  test "when we create a new size there should be items created for them automatically" do
+    assert_difference "Item.count", Product.count * (Size.count+1) do
+      @size = Size.create Factory.attributes_for(:size, :category_id => @category.id)
+    end
   end
 end
