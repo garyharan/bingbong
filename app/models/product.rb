7518816a7490e05002a4478cdc833a223098162a
Gary Haran <gary.haran@gmail.com>
2011-04-29 15:05:33 -0400

    added items if we create a product.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,10 @@ class Product < ActiveRecord::Base
 
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => :category_id
+
+  def after_create
+    category.sizes.each do |size|
+      Item.create :product_id => id, :size_id => size.id
+    end
+  end
 end
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,4 +1,15 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  setup do
+    @shop      = Factory.create :shop
+    @category  = @shop.categories.create Factory.attributes_for(:category, :shop_id      => @shop.id)
+    @size      = @category.sizes.create Factory.attributes_for(:size, :category_id => @category.id)
+  end
+
+  test "should create items when a new product is created if sizes already exist for it" do
+    assert_difference "Item.count" do
+      @size = Product.create Factory.attributes_for(:product, :category_id => @category.id)
+    end
+  end
 end
