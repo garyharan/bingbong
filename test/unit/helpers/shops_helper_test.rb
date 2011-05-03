0fc101b09700ef70f250f3ee5657cac1e7047af6
Gary Haran <gary.haran@gmail.com>
2011-05-03 16:27:17 -0400

    styling

--- test/unit/helpers/shops_helper_test.rb
+++ test/unit/helpers/shops_helper_test.rb
@@ -4,6 +4,6 @@ class ShopsHelperTest < ActionView::TestCase
   test "should create proper url" do
     @shop = Factory.build(:shop)
     @url = map_url(@shop)
-    assert_equal @url, 'http://maps.google.com/maps/api/staticmap?size=401x300&zoom=13&markers=size:mid|color:0xE35524|1514+Murray+Street%2CSaint-Hubert%2CQuebec%2CJ4T1C7&sensor=false'
+    assert_equal @url, 'http://maps.google.com/maps/api/staticmap?size=230x162&zoom=13&markers=size:mid|color:0xE35524|1514+Murray+Street%2CSaint-Hubert%2CQuebec%2CJ4T1C7&sensor=false'
   end
 end
