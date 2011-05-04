a22aa90a7c8f995ad4115c53e8ab9dba0f12076f
Gary Haran <gary.haran@gmail.com>
2011-05-04 17:37:55 -0400

    more styling.

--- app/helpers/shops_helper.rb
+++ app/helpers/shops_helper.rb
@@ -1,7 +1,7 @@
 module ShopsHelper
   def map_url(shop)
     raw [ "http://maps.google.com/maps/api/staticmap?",
-      'size=230x162', # seriously?   If I use 400x300 it blows up
+      'size=170x140', # seriously?   If I use 400x300 it blows up
       "&zoom=13",
       "&markers=size:mid|color:0xE35524|",
       CGI.escape(shop.full_address.split(',').map{|p| p.strip }.join(',')),
