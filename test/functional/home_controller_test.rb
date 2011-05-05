362ba8a9fac29ffdda151cea5c2f57a116e770e4
Gary Haran <gary.haran@gmail.com>
2011-05-05 10:50:16 -0400

    added list of shops on front page for debugging this will be useful

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class HomeControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
+    assert_not_nil assigns(:shops)
     assert_response :success
   end
 end
