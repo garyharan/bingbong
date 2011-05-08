6330203badd8a9d3db0962cba95ce46403c14ce6
Gary Haran <gary.haran@gmail.com>
2011-05-08 17:43:14 -0400

    changed Encoding call

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Bingbong::Application.initialize!
 
-Encoding.default_external = Encoding::UTF_8 
+# Encoding.default_external = Encoding::UTF_8 
