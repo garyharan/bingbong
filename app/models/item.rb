199461ad4ed2796a4405dde39834defc81659fa2
Gary Haran <gary.haran@gmail.com>
2011-05-09 14:01:05 -0400

    sugar coating

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ActiveRecord::Base
   belongs_to :product
   belongs_to :size
+
+  def name
+    product.name
+  end
 end
--- test/unit/item_test.rb
+++ test/unit/item_test.rb
@@ -1,4 +1,12 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
+  test "should provide proper name of item" do
+    @shop     = Shop.create     Factory.attributes_for(:shop)
+    @category = Category.create Factory.attributes_for(:category, :shop_id => @shop.id)
+    @product  = Product.create  Factory.attributes_for(:product, :category_id => @category.id)
+    @size     = Size.create     Factory.attributes_for(:size, :category_id => @category.id)
+    @item     = Item.create     Factory.attributes_for(:item, :product_id => @product.id, :size_id => @size.id)
+    assert_equal @product.name, @item.name
+  end
 end
