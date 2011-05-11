7649e020a7b60ead83be090666aba15597bbf466
Gary Haran <gary.haran@gmail.com>
2011-05-11 10:31:46 -0400

    Simply changing quantity instead of creating new line.

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -2,6 +2,14 @@ class LinesController < ApplicationController
   before_filter :authenticate_user!
 
   def create
-    @line = Line.create params[:line].merge(:user_id => current_user.id)
+    @shop_id = params[:line][:shop_id]
+    @item_id = params[:line][:item_id]
+
+    if @line = Line.first(:conditions => { :user_id => current_user.id, :shop_id => @shop_id, :item_id => @item_id })
+      @line.quantity += 1
+      @line.save!
+    else
+      @line = Line.create params[:line].merge(:user_id => current_user.id)
+    end
   end
 end
--- test/functional/lines_controller_test.rb
+++ test/functional/lines_controller_test.rb
@@ -18,4 +18,19 @@ class LinesControllerTest < ActionController::TestCase
     end
     assert_not_nil assigns(:line)
   end
+
+  test "should change quantity on create if a line already exists" do
+    @shop     = Shop.create     Factory.attributes_for(:shop)
+    @category = Category.create Factory.attributes_for(:category, :shop_id => @shop.id)
+    @product  = Product.create  Factory.attributes_for(:product, :category_id => @category.id)
+    @size     = Size.create     Factory.attributes_for(:size, :category_id => @category.id)
+    @item     = Item.first
+    @item.update_attribute :price, 2.99
+    @line     = Line.create(:user_id => @user.id, :shop_id => @shop.id, :item_id => @item.id)
+    assert_no_difference 'Line.count' do
+      xhr :post, :create, :line => { :shop_id => @shop.id, :item_id => @item.id }
+    end
+    assert_not_nil assigns(:line)
+    assert_equal 2, assigns(:line).quantity
+  end
 end
