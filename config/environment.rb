4699f82ae72e4d66a260d856bc9b6a93ae0add2f
Gary Haran <gary.haran@gmail.com>
2011-05-18 09:11:49 -0400

    mailer default host

--- config/environment.rb
+++ config/environment.rb
@@ -14,3 +14,6 @@ ActionMailer::Base.smtp_settings = {
   :authentication       => 'plain',
   :enable_starttls_auto => true
 }
+ActionMailer::Base.default_url_options = {
+  :host => "bingbong.ca"
+}
