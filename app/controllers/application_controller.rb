4b61906c2c87a556ebf8a9499c821642b3ecd4f3
Gary Haran <gary.haran@gmail.com>
2011-05-18 11:11:35 -0400

    flashes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # before_filter :debug
+  # def debug
+  #   flash[:alert] = "Vous devez confirmer votre compte pour continuer"
+  #   flash[:notice] = "Vous etes bien beau aujourd'hui"
+  # end
 end
