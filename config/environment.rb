a8e6d992c4faf173b972b21cdb56700902f8eb48
Gary Haran <gary.haran@gmail.com>
2011-05-22 10:41:56 -0400

    changed mail settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,17 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Bingbong::Application.initialize!
-
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'grandmenu.com',
-  :user_name            => 'gary@grandmenu.com',
-  :password             => '2bee4qABZaUz',
-  :authentication       => 'plain',
-  :enable_starttls_auto => true
-}
-ActionMailer::Base.default_url_options = {
-  :host => "grandmenu.com"
-}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,16 @@ Bingbong::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'grandmenu.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'grandmenu.com',
+    :user_name            => 'gary@grandmenu.com',
+    :password             => '2bee4qABZaUz',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
 
   # Enable threaded mode
   # config.threadsafe!
