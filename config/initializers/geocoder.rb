c6bec421de853826ed95d3a6faf918c5e2160abb
François Beausoleil <francois@teksol.info>
2011-06-29 11:11:23 -0400

    Enable me to use a blank key, which works

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,7 +1,10 @@
 Geocoder::Configuration.lookup = :google
 Geocoder::Configuration.timeout = 1
 
-if ENV["GOOGLE_MAPS_API_KEY"].blank? then
+if ENV.to_hash.has_key?("GOOGLE_MAPS_API_KEY") then
+  puts "Using ENV Google Maps API key"
+  Geocoder::Configuration.api_key = ENV["GOOGLE_MAPS_API_KEY"]
+else
   if Rails.env.production? then
     puts "Using PRODUCTION Google Maps API key"
     Geocoder::Configuration.api_key = "ABQIAAAAuCa-3icg51FeRNcYL7WhiBTF6bzZDgf24EzXlEb1urVKT1DBZxRjlpMqWAMHc4_5ga8MwX9DReM1Sw"
@@ -9,7 +12,4 @@ if ENV["GOOGLE_MAPS_API_KEY"].blank? then
     puts "Using DEVELOPMENT / TEST Google Maps API key"
     Geocoder::Configuration.api_key = "ABQIAAAAuCa-3icg51FeRNcYL7WhiBTJQa0g3IQ9GZqIMmInSLzwtGDKaBTapNNv32SFVGtU8j4NxiJA3JIwJA"
   end
-else
-  puts "Using ENV Google Maps API key"
-  Geocoder::Configuration.api_key = ENV["GOOGLE_MAPS_API_KEY"]
 end
