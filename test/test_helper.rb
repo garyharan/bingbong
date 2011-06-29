aa51e93b066822db969d4446359638eb5c02d16e
François Beausoleil <francois@teksol.info>
2011-06-29 10:32:00 -0400

    Ensure this is loaded when possible

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,12 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'factory_girl_rails'
 
+begin
+  require "ruby-debug"
+rescue LoadError
+  # NOP - ignore
+end
+
 Dir[Rails.root.join("factories/*.rb")].each { |f| require f }
 
 class ActionController::TestCase
