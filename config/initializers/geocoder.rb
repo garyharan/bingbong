da283aab912360c1ea8503433fb75803c4bbebfc
François Beausoleil <francois@teksol.info>
2011-06-29 10:46:30 -0400

    Enable setting the key from the environment

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,8 +1,15 @@
 Geocoder::Configuration.lookup = :google
 Geocoder::Configuration.timeout = 1
 
-if ENV['RAILS_ENV'] == 'production'
-  Geocoder::Configuration.api_key = "ABQIAAAAuCa-3icg51FeRNcYL7WhiBTF6bzZDgf24EzXlEb1urVKT1DBZxRjlpMqWAMHc4_5ga8MwX9DReM1Sw"
+if ENV["GOOGLE_MAPS_API_KEY"].blank? then
+  if Rails.env.production? then
+    puts "Using PRODUCTION Google Maps API key"
+    Geocoder::Configuration.api_key = "ABQIAAAAuCa-3icg51FeRNcYL7WhiBTF6bzZDgf24EzXlEb1urVKT1DBZxRjlpMqWAMHc4_5ga8MwX9DReM1Sw"
+  else
+    puts "Using DEVELOPMENT / TEST Google Maps API key"
+    Geocoder::Configuration.api_key = "ABQIAAAAuCa-3icg51FeRNcYL7WhiBTJQa0g3IQ9GZqIMmInSLzwtGDKaBTapNNv32SFVGtU8j4NxiJA3JIwJA"
+  end
 else
-  Geocoder::Configuration.api_key = "ABQIAAAAuCa-3icg51FeRNcYL7WhiBTJQa0g3IQ9GZqIMmInSLzwtGDKaBTapNNv32SFVGtU8j4NxiJA3JIwJA"
+  puts "Using ENV Google Maps API key"
+  Geocoder::Configuration.api_key = ENV["GOOGLE_MAPS_API_KEY"]
 end
