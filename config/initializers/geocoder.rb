db6dcecea68818dbc484e8dddf6ee6b698c126f5
Gary Haran <gary@openplaces.com>
2011-07-28 10:26:07 -0400

    now using geocoder.ca API instead of google maps

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,15 +1,5 @@
-Geocoder::Configuration.lookup = :google
-Geocoder::Configuration.timeout = 1
+Geocoder::Configuration.lookup = :geocoder_ca
+Geocoder::Configuration.timeout = 3
 
-if ENV.to_hash.has_key?("GOOGLE_MAPS_API_KEY") then
-  puts "Using ENV Google Maps API key"
-  Geocoder::Configuration.api_key = ENV["GOOGLE_MAPS_API_KEY"]
-else
-  if Rails.env.production? then
-    puts "Using PRODUCTION Google Maps API key"
-    Geocoder::Configuration.api_key = "ABQIAAAAuCa-3icg51FeRNcYL7WhiBTF6bzZDgf24EzXlEb1urVKT1DBZxRjlpMqWAMHc4_5ga8MwX9DReM1Sw"
-  else
-    puts "Using DEVELOPMENT / TEST Google Maps API key"
-    Geocoder::Configuration.api_key = "ABQIAAAAuCa-3icg51FeRNcYL7WhiBTJQa0g3IQ9GZqIMmInSLzwtGDKaBTapNNv32SFVGtU8j4NxiJA3JIwJA"
-  end
-end
+puts "Using geocoder.ca key."
+Geocoder::Configuration.api_key = "686674307166573397008x1696"
