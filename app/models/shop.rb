34d2683cc2367495aa3601c8fd18c162149b7baf
François Beausoleil <francois@teksol.info>
2011-08-05 21:35:13 -0400

    Guard against nil

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -6,7 +6,7 @@ class Shop < ActiveRecord::Base
   geocoded_by :full_address
   after_validation :geocode
   before_validation do
-    normalized = self.postal_code.upcase.gsub(/[^A-Z0-9]/, "")
+    normalized = self.postal_code.to_s.upcase.gsub(/[^A-Z0-9]/, "")
     normalized = "#{normalized[0, 3]} #{normalized[3, 3]}"
     self.postal_code = normalized
   end
