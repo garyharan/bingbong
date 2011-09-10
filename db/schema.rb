242fc591d7bf08caa45ccca8f2e3c81c6f9cc121
Gary Haran <gary@openplaces.com>
2011-09-10 15:39:21 -0400

    added description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110910145139) do
+ActiveRecord::Schema.define(:version => 20110910191045) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(:version => 20110910145139) do
     t.integer  "delivery",                                   :default => 30
     t.float    "latitude"
     t.float    "longitude"
+    t.text     "description"
   end
 
   create_table "sizes", :force => true do |t|
