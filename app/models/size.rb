040413401f94ffe08ccc5f9c15871395e4a0a3b1
Martin Provencher <mprovencher86@gmail.com>
2011-09-11 11:32:17 -0400

    Convert quote and double quotes into 'pieds' and 'pouce'

--- app/models/size.rb
+++ app/models/size.rb
@@ -1,4 +1,6 @@
 class Size < ActiveRecord::Base
+  include ActionView::Helpers::TextHelper
+
   belongs_to :category
   has_many :products, :through => :items
 
@@ -12,5 +14,16 @@ class Size < ActiveRecord::Base
   before_destroy :destroy_related_items
   def destroy_related_items
     Item.destroy_all :size_id => id
+  end
+
+  def order_name
+    @order_name ||= name.gsub(/(\d+)\s*(['"])/) do |match|
+      case $2
+        when "'"
+          pluralize($1, "pied")
+        when '"'
+          pluralize($1, "pouce")
+      end
+    end
   end
 end
