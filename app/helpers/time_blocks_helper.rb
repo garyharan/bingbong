f7f122fdba82d6c44b2888ce77899f3a62eb6bd0
Gary Haran <gary@openplaces.com>
2011-09-11 14:50:41 -0400

    making time block week days presentable.

--- app/helpers/time_blocks_helper.rb
+++ app/helpers/time_blocks_helper.rb
@@ -1,2 +1,8 @@
 module TimeBlocksHelper
+
+  WEEKDAYS = %w(Lundi Mardi Mercredi Jeudi Vendredi Samedi Dimanche)
+
+  def human_weekday(weekday)
+    WEEKDAYS[weekday - 1]
+  end
 end
--- test/unit/helpers/time_blocks_helper_test.rb
+++ test/unit/helpers/time_blocks_helper_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
 class TimeBlocksHelperTest < ActionView::TestCase
+  def test_weekday
+    assert_equal "Lundi"    , human_weekday(1)
+    assert_equal "Mardi"    , human_weekday(2)
+    assert_equal "Mercredi" , human_weekday(3)
+    assert_equal "Jeudi"    , human_weekday(4)
+    assert_equal "Vendredi" , human_weekday(5)
+    assert_equal "Samedi"   , human_weekday(6)
+    assert_equal "Dimanche" , human_weekday(7)
+  end
 end
