b24dfb11b86de9a0c99236c39823b9227187a359
Gary Haran <gary@openplaces.com>
2011-10-16 14:51:09 -0400

    fixed the bug with weekdays.  Sundays now treated properly with index 0.

--- app/helpers/time_blocks_helper.rb
+++ app/helpers/time_blocks_helper.rb
@@ -1,6 +1,6 @@
 module TimeBlocksHelper
 
-  WEEKDAYS = %w(Lundi Mardi Mercredi Jeudi Vendredi Samedi Dimanche)
+  WEEKDAYS = %w(Dimanche Lundi Mardi Mercredi Jeudi Vendredi Samedi)
 
   def human_weekday(weekday)
     WEEKDAYS[weekday - 1]
