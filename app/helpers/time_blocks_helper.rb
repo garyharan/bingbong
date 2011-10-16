484a38f541d1345c63d230d097e6fef2a54f30b2
Gary Haran <gary@openplaces.com>
2011-10-16 17:55:16 -0400

    fixed test with time blocks

--- app/helpers/time_blocks_helper.rb
+++ app/helpers/time_blocks_helper.rb
@@ -3,6 +3,6 @@ module TimeBlocksHelper
   WEEKDAYS = %w(Dimanche Lundi Mardi Mercredi Jeudi Vendredi Samedi)
 
   def human_weekday(weekday)
-    WEEKDAYS[weekday - 1]
+    WEEKDAYS[weekday]
   end
 end
